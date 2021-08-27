6b69e97d3340560e6257565f5806c611580e00a4
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-08-27 18:49:09 -0300

    Implement resource hotel
    Implement resource hotel and refactor app

---
+++ resources/hotel.py
@@ -0,0 +1,46 @@
+from flask_restful import Resource
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome':'Alha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome':'Bravo Hotel',
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+    {
+        'hotel_id': 'chalie',
+        'nome':'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 320.20,
+        'cidade': 'Santa Catarina'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'Hoteis': hoteis}
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message':'Hotel Not Found.'}, 404 #not found
+
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
+
